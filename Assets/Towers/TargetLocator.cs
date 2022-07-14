d10fb7349577a79cf66f6c448fc9448e803885b8
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-14 13:49:53 +0200

    #125: Add range and closest target to tower

--- Assets/Towers/TargetLocator.cs
+++ Assets/Towers/TargetLocator.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] GameObject weapon;
+    [SerializeField] float range = 15f;
     
-    GameObject target;
-
-    void Start()
-    {
-        target = FindObjectOfType<Mover>().gameObject;
-    }
+    Transform target;
+    float distance;
 
     void Update()
     {
+        FindClosestEnemy();
+        AimWeapon();
+        Shoot();
+    }
+
+    void AimWeapon()
+    {
         weapon.transform.LookAt(target.transform.position);
+    }
+
+    void Shoot()
+    {
+        bool isShooting = distance <= range;
+        var em = weapon.GetComponentInChildren<ParticleSystem>().emission;
+        em.enabled = isShooting;
+    }
+
+    void FindClosestEnemy()
+    {
+        Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < maxDistance)
+            {
+                maxDistance = distance;
+                closestTarget = enemy.transform;
+            }
+        }
+
+        target = closestTarget;
+        distance = maxDistance;
     }
 }
