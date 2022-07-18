a5f04e641d9471fa722ee1a3153dce87f734aaef
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-18 19:26:08 +0200

    #139: Add BreadFirstSearch

--- Assets/Pathfinding/Pathfinder.cs
+++ Assets/Pathfinding/Pathfinder.cs
@@ -4,32 +4,53 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] Node currentSearchNode;
+    [SerializeField] Vector2Int startCoordinate;
+    [SerializeField] Vector2Int endCoordinate;
+
+    Node startNode;
+    Node endNode;
+    Node currentSearchNode;
+
     GridManageer gridManageer;
     Vector2Int[] directionOrder = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> searched = new Dictionary<Vector2Int, Node>();
 
     void Start()
     {
         gridManageer = FindObjectOfType<GridManageer>();
         if (gridManageer != null)
         {
-            currentSearchNode = gridManageer.GetNode(currentSearchNode.coordinates);
-            currentSearchNode.isPath = true;
+            startNode = gridManageer.GetNode(startCoordinate);
+            endNode   = gridManageer.GetNode(endCoordinate);
         }
-        ExploreNeighbors();
+        BreadFirstSearch();
     }
 
     void ExploreNeighbors()
     {
-        List<Node> neighbors = new List<Node>();
-
         foreach (Vector2Int dir in directionOrder)
         {
             Node node = gridManageer.GetNode(currentSearchNode.coordinates + dir);
-            if (node == null)
+            if (node == null || searched.ContainsKey(node.coordinates))
                 continue;
-            node.isExplored = true;
-            neighbors.Add(node);
+            frontier.Enqueue(node);
+        }
+    }
+
+    void BreadFirstSearch()
+    {
+        frontier.Enqueue(startNode);
+        currentSearchNode = startNode;
+
+        while (frontier.Count > 0 && currentSearchNode.coordinates != endCoordinate)
+        {
+            currentSearchNode = frontier.Dequeue();
+            ExploreNeighbors();
+            if (!searched.ContainsKey(currentSearchNode.coordinates))
+                searched.Add(currentSearchNode.coordinates, currentSearchNode);
+            currentSearchNode.isExplored = true;
         }
     }
 }
